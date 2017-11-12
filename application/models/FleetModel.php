756013c57c0a7b7bd437e967da623087661eb019
Teresa Guan <tguan1@my.bcit.ca>
2017-11-12 15:22:09 -0800

    Sync with team repo.

--- application/models/FleetModel.php
+++ application/models/FleetModel.php
@@ -18,4 +18,21 @@ class FleetModel extends CSV_Model
     public function saveFleet($entity){
         
     }
+    
+    // provide form validation rules
+    public function rules()
+    {
+        $config = array(
+            ['field' => 'task', 'label' => 'TODO task', 'rules' => 'alpha_numeric_spaces|max_length[64]'],
+            ['field' => 'priority', 'label' => 'Priority', 'rules' => 'integer|less_than[4]'],
+            ['field' => 'size', 'label' => 'Task size', 'rules' => 'integer|less_than[4]'],
+            ['field' => 'group', 'label' => 'Task group', 'rules' => 'integer|less_than[5]'],
+        );
+            return $config;
+    }
+    // retrieve a single plane, null if not found
+//    public function get($which);
+
+    // retrieve all of the planes
+//    public function all();
 }
