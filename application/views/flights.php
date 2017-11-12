fefe745ae8e5849462bd090e8c0c5a8fd2037a72
Hiram Cabrera <takitocabrera@gmail.com>
2017-11-12 02:40:49 -0800

    Basic layout has been created but unable to build function that updates csv

--- application/views/flights.php
+++ application/views/flights.php
@@ -1,23 +1,25 @@
 <div class="container-fluid">
     <div class="jumbotron">
-        <table id="flights">
-            <tr>
-                <th>Flight ID</th>
-                <th>Departure Airport</th>
-                <th>Departure Time</th>
-                <th>Arrival Airport</th>
-                <th>Arrive Time</th>
-            </tr>
-            {flightdata}
-            <tr>
-                <td title="{plane}">{id}</td>
-                <td title="{depart_airport}">{depart}</td>
-                <td>{depart_time}</td>
-                <td title="{arrival_airport}">{arrival}</td>
-                <td>{arrival_time}</td>
-            </tr>
-            {/flightdata}
-        </table>
+        <div class="table-responsive">
+            <table class="table">
+                <tr>
+                    <th>Flight ID</th>
+                    <th>Departure Airport</th>
+                    <th>Departure Time</th>
+                    <th>Arrival Airport</th>
+                    <th>Arrive Time</th>
+                </tr>
+                {flightdata}
+                <tr>
+                    <td title="{plane}">{id}</td>
+                    <td title="{depart_airport}">{depart}</td>
+                    <td>{depart_time}</td>
+                    <td title="{arrival_airport}">{arrival}</td>
+                    <td>{arrival_time}</td>
+                </tr>
+                {/flightdata}
+            </table>
+        </div>
     </div>
 </div>
 
